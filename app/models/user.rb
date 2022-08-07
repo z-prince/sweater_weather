90383a982d5dc532105de6dd968f7d0703e6e7b8
Zach Prince <zachary.s.prince@outlook.com>
2022-08-07 11:53:31 -0400

    Add user api key association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password
 
+  has_many :api_keys, as: :bearer
+
   has_secure_password
 end
--- db/migrate/20220807153925_create_users.rb
+++ db/migrate/20220807153925_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :create_users do |t|
-      t.string :email
-      t.string :password_digest
-
-      t.timestamps
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.timestamps null: false
     end
+
+    add_index :users, :email, unique: true
   end
 end
