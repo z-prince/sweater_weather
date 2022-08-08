21efc997de4d4219a4fc9d06d5cd642630e6e875
Zach Prince <zachary.s.prince@outlook.com>
2022-08-08 13:05:14 -0400

    Add sad path tests and logic

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -2,10 +2,16 @@ module Api
   module V1
     class MunchiesController < ApplicationController
       def index
-        forecast = ForecastFacade.create_forecast(params[:location])
-        restaurant = RestaurantFacade.create_restaurant(params[:location], params[:food])
+        if params[:food].blank?
+          render json: { errors: { data: 'missing food parameter' } }, status: 400
+        elsif params[:location].blank?
+          render json: { errors: { data: 'missing location parameter' } }, status: 400
+        else
+          forecast = ForecastFacade.create_forecast(params[:location])
+          restaurant = RestaurantFacade.create_restaurant(params[:location], params[:food])
 
-        render json: RestaurantSerializer.format_restaurant(forecast, restaurant)
+          render json: RestaurantSerializer.format_restaurant(forecast, restaurant)
+        end
       end
     end
   end
--- spec/requests/ap1/v1/restaurant_request_spec.rb
+++ spec/requests/ap1/v1/restaurant_request_spec.rb
@@ -1,26 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe 'The restaurant API', :vcr do
-  it 'Returns the restaurant for a specific city and category', :vcr do
-    get '/api/v1/munchies?location=denver,co&food=chinese'
+  describe 'happy path' do
+    it 'Returns the restaurant for a specific city and category', :vcr do
+      get '/api/v1/munchies?location=denver,co&food=chinese'
 
-    response_body = JSON.parse(response.body, symbolize_names: true)
+      response_body = JSON.parse(response.body, symbolize_names: true)
 
-    restaurant_data = response_body[:data]
+      restaurant_data = response_body[:data]
 
-    forecast = restaurant_data[:attributes][:forecast]
-    restaurant = restaurant_data[:attributes][:restaurant]
+      forecast = restaurant_data[:attributes][:forecast]
+      restaurant = restaurant_data[:attributes][:restaurant]
 
-    expect(response).to be_successful
-    expect(restaurant_data[:id]).to eq('null')
-    expect(restaurant_data[:type]).to eq('munchie')
-    expect(restaurant_data[:attributes].keys).to eq(%i[destination_city forecast restaurant])
+      expect(response).to be_successful
+      expect(restaurant_data[:id]).to eq('null')
+      expect(restaurant_data[:type]).to eq('munchie')
+      expect(restaurant_data[:attributes].keys).to eq(%i[destination_city forecast restaurant])
 
-    ##### Forecast expectations #####
+      ##### Forecast expectations #####
 
-    expect(restaurant_data[:attributes][:forecast].keys).to eq(%i[summary temperature])
-    ##### Restaurant expectations #####
+      expect(restaurant_data[:attributes][:forecast].keys).to eq(%i[summary temperature])
+      ##### Restaurant expectations #####
 
-    expect(restaurant_data[:attributes][:restaurant].keys).to eq(%i[name address])
+      expect(restaurant_data[:attributes][:restaurant].keys).to eq(%i[name address])
+    end
+  end
+  describe 'sad path' do
+    it 'returns a message for missing params when params are missing' do
+      get '/api/v1/munchies?location=denver,co'
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:errors][:data]).to eq('missing food parameter')
+    end
+
+    it 'returns a message for missing location param' do
+      get '/api/v1/munchies?food=chinese'
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:errors][:data]).to eq('missing location parameter')
+    end
   end
 end
