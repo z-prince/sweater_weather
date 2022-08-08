0a58899fe857c657f27982b7055d46b4127c556b
Zach Prince <zachary.s.prince@outlook.com>
2022-08-08 12:23:59 -0400

    Add join method to address

--- app/poros/restaurant.rb
+++ app/poros/restaurant.rb
@@ -4,6 +4,8 @@ class Restaurant
 
   def initialize(restaurant_data)
     @name = restaurant_data[:businesses][0][:name]
-    @address = restaurant_data[:businesses][0][:location][:display_address]
+    @address = restaurant_data[:businesses][0][:location][:display_address].join
+    @city = restaurant_data[:businesses][0][:location][:city]
+    @state = restaurant_data[:businesses][0][:location][:state]
   end
 end
