40d29ac0831c05b764a302096612bfba5e7e9426
Zach Prince <zachary.s.prince@outlook.com>
2022-08-08 12:44:29 -0400

    Add munchies route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      resources :munchies, only: :index
       resources :sessions, only: :create
       resources :users, only: :create
       resources :forecast, only: :index
