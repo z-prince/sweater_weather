5db65bd8ef36e2343203ea7e4762a43d70a310bd
Zach Prince <zachary.s.prince@outlook.com>
2022-08-09 11:27:56 -0400

    Add user create test

--- spec/requests/ap1/v1/users_request_spec.rb
+++ spec/requests/ap1/v1/users_request_spec.rb
@@ -3,11 +3,23 @@ require 'rails_helper'
 RSpec.describe 'User requests' do
   describe 'create user' do
     it 'can create a user', :vcr do
-      post '/api/v1/users', params: {
-        email: 'coolguy27@aol.com',
-        password: 'secret',
-        password_confirmation: 'secret'
-      }, as: :json
+      params = {
+        'email': 'coolguy23@aol.com',
+        'password': 'verysecret',
+        'password_confirmation': 'verysecret'
+      }
+
+      headers = {
+        'Content_Type': 'application/json',
+        'Accept': 'application/json'
+      }
+
+      post('/api/v1/users', headers: headers, params: params.to_json)
+
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(User.all.first.email).to eq('coolguy23@aol.com')
     end
   end
 end
