ce6b7b5fea060dd0f904893690ce2df074a0c706
Zach Prince <zachary.s.prince@outlook.com>
2022-08-09 12:20:19 -0400

    Add sessions controller and login method

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      def create
+        user = User.find_by(email: user_params[:email])
+        if !user.nil?
+          session[:id] = user[:id]
+          render json: UserSerializer.new(user), status: :created
+        else
+          render json: { errors: 'Invalid credentails' }
+        end
+      end
+
+      private
+
+      def user_params
+        JSON.parse(request.body.read, symbolize_names: true)
+      end
+    end
+  end
+end
